12d4117efff41acf5113dd6a69bdd67e36fb05fd
Eleni <helena.aslanidou@gmail.com>
2022-03-21 14:13:39 +0100

    build

---
+++ build/precache-manifest.fb1e59909ec63c6a3f55c3a6aea1577e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5087c90d54ac59eb2905356cdbeee26d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b00a94ddddec48d2e3ad",
+    "url": "/static/css/2.a6bc1c31.chunk.css"
+  },
+  {
+    "revision": "b1c4976af8cd034fb3d3",
+    "url": "/static/css/main.f4b7f953.chunk.css"
+  },
+  {
+    "revision": "b00a94ddddec48d2e3ad",
+    "url": "/static/js/2.f0ce1280.chunk.js"
+  },
+  {
+    "revision": "c7abd4a823ee85afac2f05677368b7de",
+    "url": "/static/js/2.f0ce1280.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b1c4976af8cd034fb3d3",
+    "url": "/static/js/main.117ab051.chunk.js"
+  },
+  {
+    "revision": "a115e4e0014e98da6408",
+    "url": "/static/js/runtime-main.451e9585.js"
+  }
+]);
